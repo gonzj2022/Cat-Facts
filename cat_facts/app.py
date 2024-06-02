cb73398bde401a1d9596a66f1440a8564bde58fc
gonzj988 <gonzj988@hsd.k12.or.us>
2024-06-01 22:32:07 -0700

    Got label working and it prints out results.

--- cat_facts/app.py
+++ cat_facts/app.py
@@ -24,7 +24,8 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtCore import Qt
 
 class MainWindow(QMainWindow):
-
+    global catfacts
+    catfacts = ""
     def __init__(self):
         super(MainWindow, self).__init__()
 
@@ -41,20 +42,38 @@ class MainWindow(QMainWindow):
         label1 = QLabel("Would you like to learn about Cats Facts or Breeds?")
         layout.addWidget(label1)
 
-        self.Options1 = QComboBox()
-        self.Options1.addItems(["Facts","Breeds"])
-        layout.addWidget(self.Options1)
+        self.Options = QComboBox()
+        self.Options.addItems(["Facts","Breeds"])
+        layout.addWidget(self.Options)
 
         label2 = QLabel("Choose how many?")
         layout.addWidget(label2)
 
-        self.Options2 = QComboBox()
-        self.Options2.addItems(["1","2","3","4","5"])
-        layout.addWidget(self.Options2)
+        self.qty = QComboBox()
+        self.qty.addItems(["1","2","3","4","5"])
+        layout.addWidget(self.qty)
+       
+        self.user_sel_button = QPushButton(text="Submit Selections")
+        layout.addWidget(self.user_sel_button)
+        self.user_sel_button.clicked.connect(self.call_controller)
+                
+        label3 = QLabel("Results:")
+        layout.addWidget(label3)
+        
+        self.result0 = QLabel("")
+        layout.addWidget(self.result0)
 
-        self.PB1 = QPushButton(text="Sumbit Selections")
-        layout.addWidget(self.PB1)
-        self.PB1.clicked.connect(self.call_controller)
+        self.result1 = QLabel("")
+        layout.addWidget(self.result1)
+
+        self.result2 = QLabel("")
+        layout.addWidget(self.result2)
+
+        self.result3 = QLabel("")
+        layout.addWidget(self.result3)
+
+        self.result4 = QLabel("")
+        layout.addWidget(self.result4)
 
         widget = QWidget()
         widget.setLayout(layout)
@@ -63,10 +82,32 @@ class MainWindow(QMainWindow):
         # to take up all the space in the window by default.
         self.setCentralWidget(widget)
 
+    def clear_labels(self):
+            self.result0.setText("")
+            self.result1.setText("")
+            self.result2.setText("")
+            self.result3.setText("")
+            self.result4.setText("")
+            
     def call_controller(self):
-        user_sel1 = self.Options1.currentText().lower()
-        user_sel2 = self.Options2.currentIndex() + 1
-        facts = controller.catfact(user_sel1, user_sel2)
+        self.clear_labels()
+        user_sel1 = self.Options.currentText().lower()
+        user_sel2 = self.qty.currentIndex() + 1
+        catfacts = controller.catfact(user_sel1, user_sel2)
+        catfactslist = catfacts.split('\n')
+        x = 0
+        for x in range(user_sel2):
+            if x == 0:
+                self.result0.setText(catfactslist[0])
+            if x == 1:
+                self.result1.setText(catfactslist[1])
+            if x == 2:
+                self.result2.setText(catfactslist[2])
+            if x == 3:
+                self.result3.setText(catfactslist[3])
+            if x == 4:
+                self.result4.setText(catfactslist[4])    
+            x = x+1
 
 app = QApplication(sys.argv)
 w = MainWindow()
--- cat_facts/controller.py
+++ cat_facts/controller.py
@@ -9,20 +9,26 @@ import requests as re
 # Global scope
 def catfact(selection, num):
     facts = ""
-    url = f"https://catfact.ninja/{selection}?max_lenght=200&limit=" + str(num)
+    url = f"https://catfact.ninja/{selection}?limit=" + str(num)
+    print("url:" +url)
     response = re.get(url)
     if response.ok:
-        facts = get_facts(response)
+        facts = get_facts(selection,response)
     else:
         facts = f"There was an error: {response.status_code}"
     return facts
 
 
-def get_facts(response):
+def get_facts(selection,response):
     output = ""
     response_json = response.json()
     factlist = response_json["data"]
-    for item in factlist:
-        fact = item["fact"]
-        output += fact + "/n"
+    if selection == "facts":
+        for item in factlist:
+            fact = item["fact"]
+            output += fact + "\n"
+    if selection == "breeds":
+        for item in factlist:
+            fact = item["breed"]
+            output += fact + "\n"
     return output
